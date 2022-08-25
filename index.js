1b4010acbc1e813a99d436acdc6fcdde744a6e1f
mohadmin25 <abuhamar008@gmail.com>
2022-08-25 19:59:42 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", () => {
+    let manipulate = document.querySelector('#text')
+    manipulate.textContent = 'This is really cool!'
+    console.log("The DOM has loaded");
+})
